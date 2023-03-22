224c3a52d652ee1281a372717b634dd8b2dd6c1d
bobevans <caiwendi88@gmail.com>
2023-03-23 00:23:15 +0800

    fix(1): 优化结构减少意外
    fix(1): 优化结构减少意外

--- vendors/client/index.js
+++ vendors/client/index.js
@@ -1,13 +1,17 @@
-import "./styles/common.scss";
-import "./styles/theme.less";
-import { ConfigProvider } from "antd";
-import "./plugin";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Application";
 import { Provider } from "react-redux";
 import createStore from "./reducer/create";
-
+import { ConfigProvider } from "antd";
+import "./plugin";
+// style
+import "./styles/custom-ui/index.less";
+//
+import "./styles/antd-ui/index.less";
+//
+import "./styles/material-ui/index.scss";
 // 由于 antd 组件的默认文案是英文，所以需要修改为中文
 // import zhCN from "antd/lib/locale-provider/zh_CN";
 
