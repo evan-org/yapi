db12c8eb000682dfc211fe29e0197470b70c813d
bobevans <caiwendi88@gmail.com>
2023-03-25 19:39:31 +0800

    调整文件结构

--- vendors/build/webpack.dev.js
+++ vendors/build/webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = {
       {
         context: ["/api", "/login"],
         // 转发端口自定义
-        target: "http://127.0.0.1:3000",
+        target: "http://127.0.0.1:3030",
         ws: true,
       },
     ],
