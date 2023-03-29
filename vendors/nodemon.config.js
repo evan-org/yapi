127ccf8479e4abb64f384d1ac97196a7db954853
caiwendi88@gmail.com <caiwendi88@gmail.com>
2023-03-29 17:52:35 +0800

    替换依赖

---
+++ vendors/nodemon.config.js
@@ -0,0 +1,31 @@
+const { exec } = require("child_process");
+
+// 获取当前环境
+const env = process.env.NODE_ENV || "development";
+console.log(env);
+
+// 根据环境加载相应的配置文件
+const nodemonConfig = require(`./nodemon.${env}.json`);
+
+// 启动应用程序
+const app = exec(`nodemon -L -C ${nodemonConfig.exec}`, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
+
+// 监听应用程序的输出
+app.stdout.on("data", (data) => {
+  console.log(data.toString());
+});
+
+app.stderr.on("data", (data) => {
+  console.error(data.toString());
+});
+
+app.on("exit", (code) => {
+  console.log(`Application exited with code ${code}`);
+});
