d582bc255c274bb2c698298a54f0968cad11aac2
BOBEVANS-PC\caiwe <caiwendi88@gmail.com>
2023-07-12 23:56:22 +0800

    统一结构

--- vendors/client/index.jsx
+++ vendors/client/index.jsx
@@ -1,4 +1,6 @@
 import "./plugin";
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "@/dev/index.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 // third party
@@ -18,7 +20,10 @@ const root = ReactDOM.createRoot(document.getElementById("yapi"));
 root.render(
   <Provider store={store}>
     <BrowserRouter basename={config.basename}>
-      <App/>
+      <DevSupport ComponentPreviews={ComponentPreviews}
+        useInitialHook={useInitial}>
+        <App/>
+      </DevSupport>
     </BrowserRouter>
   </Provider>
 );
